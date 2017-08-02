1d75bff75b59de184bd32ced4e17e69722ba3205
Olexandr Leuschenko <olexa.le@gmail.com>
2017-08-02 22:48:10 +0300

    Add project files.

---
+++ CounterApp/CounterApp/Counter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CounterApp
+{
+    internal class CounterReducer
+    {
+        public CounterState Reduce(CounterState state, object action)
+        {
+            switch (action)
+            {
+                case IncrementAction i:
+                    return ProcessIncrement(state);
+
+                case DecrementAction d:
+                    return ProcessDecrement(state);
+
+                default:
+                    throw new ArgumentException("action is not supported");
+            }
+        }
+
+        private CounterState ProcessDecrement(CounterState state)
+        {
+            return new CounterState(state.Count - 1);
+        }
+
+        private CounterState ProcessIncrement(CounterState state)
+        {
+            return new CounterState(state.Count + 1);
+        }
+    }
+
+    internal class CounterState
+    {
+        public CounterState(int count)
+        {
+            Count = count;
+        }
+
+        public int Count { get; }
+    }
+
+    internal class CounterStore : INotifyPropertyChanged
+    {
+        private readonly CounterReducer reducer = new CounterReducer();
+        private CounterState state;
+
+        public CounterStore(CounterState initialState)
+        {
+            state = initialState;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CounterState State
+        {
+            get { return state; }
+            private set { state = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(State))); }
+        }
+
+        public void Dispatch(object action)
+        {
+            State = reducer.Reduce(State, action);
+        }
+    }
+
+    internal class DecrementAction
+    { }
+
+    internal class IncrementAction
+    { }
+}
---
+++ CounterApp/CounterApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CounterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private CounterStore store;
+
+        public MainPage()
+        {
+            store = new CounterStore(new CounterState(0));
+            BindingContext = store;
+            InitializeComponent();
+        }
+
+        private void Decrement_Clicked(object sender, EventArgs e)
+        {
+            store.Dispatch(new DecrementAction());
+        }
+
+        private void Increment_Clicked(object sender, EventArgs e)
+        {
+            store.Dispatch(new IncrementAction());
+        }
+    }
+}
